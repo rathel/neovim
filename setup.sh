8e0f58598b2f2d012c1abdbb4030db47b632638f
rathel <rathel@debian-desktop>
2023-08-04 21:08:26 -0600

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,17 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 #
 if [ -d ~/.config/nvim ];then
 	rm -vfr ~/.config/nvim
 	mkdir ~/.config/nvim
+		else
+			mkdir ~/.config/nvim
 fi
 if [ -d ~/.local/share/nvim ];then
 	rm -vfr ~/.local/share/nvim
 	git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 		~/.local/share/nvim/site/pack/packer/start/packer.nvim
+		else
+			git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+				~/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
-cp -vfr * ~/.config/nvim/
+ln -s $(pwd) ~/.config/nvim
